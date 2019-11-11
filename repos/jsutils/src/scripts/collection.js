3c8520bea6d6e65221ed0780e5a82d6887324732
Lance Tipton <lancetipton04@gmail.com>
2019-11-11 09:54:08 -0700

    Collection - shallowEqual (#14)
    * Updates
      * Added shallowEqual helper
        * Compares the keys values of two collections
    
    * Updates
      * Removed . requirment for path string
    
    * Updates
      * removed col2 check, cause both collections keys could be falsy

--- repos/jsutils/src/scripts/collection.js
+++ repos/jsutils/src/scripts/collection.js
@@ -5,6 +5,7 @@
 import { isFunc } from './method'
 import { isArr } from './array'
 import { isNum } from './number'
+import { isStr } from './string'
 
 /**
  * Updates a collection by removing, getting, adding to it.
@@ -276,4 +277,57 @@ export const repeat = (element, times, cloneDeep=false) => {
     arr.push(value)
   }
   return arr
+}
+
+
+/**
+ * Compares a collection's keys / values with another collections keys / values
+ * @example
+ * shallowEqual({ foo: 'bar' }, { foo: 'bar' })
+ * // Returns true
+ * @example
+ * shallowEqual({ foo: 'bar', baz: {} }, { foo: 'bar', baz: {} })
+ * // Returns false, because the baz values are different objects
+ * @example
+ * // Works with array too
+ * shallowEqual([ 1, 2 ], [ 1, 2 ])
+ * // Returns true
+ * @example
+ * shallowEqual([{ foo: 'bar' }], [{ foo: 'bar' }])
+ * // Returns false, because the objects in index 0 are different
+ * @example
+ * // Pass a path to compare instead of the root
+ * shallowEqual({ foo: { bar: { baz: 'biz' }}}, { foo: { bar: { baz: 'biz' }}}, 'foo.bar')
+ * // Returns true, because the bar object is compared
+ * @function
+ * @param {Object|Array} col1 - Collection to compare
+ * @param {Object|Array} col2 - Collection to compare
+ * @param {Array|string} path - path of object to compare. Uses the get method to find the path
+ *
+ * @returns {boolean} - true or false if the objects keys values are equal
+ */
+export const shallowEqual = (col1, col2, path) => {
+
+  // If a path is passed in, update the collections to be that path
+  if(path && (isArr(path) || isStr(path))){
+    col1 = get(col1, path)
+    col2 = get(col2, path)
+  }
+  
+  // If the objects are the same, so return true
+  if(col1 === col2) return true
+
+  // Ensure the objects exist, and they have keys we can compare
+  if (typeof col1 !== "object" || !col1 || typeof col2 !== "object" || !col2)
+    return false
+
+  // If they have different key lengths, then they are not equal
+  if (Object.keys(col1).length !== Object.keys(col2).length) return false
+  
+  // Loop the keys, and ensure the other collection has the key and it's value is the same
+  for (const key in col1)
+    if (col1[key] !== col2[key]) return false
+
+  // Keys and values are equal, so return true
+  return true
 }
