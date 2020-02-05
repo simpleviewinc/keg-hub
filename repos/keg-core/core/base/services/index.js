54bfc62638c44e3627c4e9d6407f225ae874f426
Lance Tipton <lancetipton04@gmail.com>
2020-02-05 12:19:24 -0700

    Merge pull request #3 from mpcarolin/ZOP-101/firebase-collections-init
    Updates for tap-events-force

--- repos/keg-core/core/base/services/index.js
+++ repos/keg-core/core/base/services/index.js
@@ -1,5 +1,6 @@
 // App services
 export * from 'SVServices/networkRequest'
+export * from 'SVServices/tapIndex'
 import * as nativeService from 'SVServices/nativeService'
 
 export { nativeService }
