76f7a0d116364072130b0c9cc2a37f3ea5346de5
lancetipton <lancetipton04@gmail.com>
2020-05-21 17:48:35 -0700

    Updates   * Added kegdev url to virtualIP log   * Removed yarn deps install from core dockerfile   * Added image to compose files, to properly name the images

--- src/utils/log/logVirtualIP.js
+++ src/utils/log/logVirtualIP.js
@@ -6,10 +6,16 @@ const { Logger } = require('KegLog')
  */
 const logVirtualIP = async () => {
   const vmIP = await getVirtualIP()
+
   Logger.empty()
+  console.log(
+    Logger.colors.cyan(` Application is being served at: `),
+    Logger.colors.brightWhite(`http://tap.kegdev.xyz/`),
+  )
   Logger.info(` Virtual Machine's IP Address: http://${vmIP}`)
-  Logger.warn(` Use this IP in your browser. NOT the IP provided by Expo!`)
+  Logger.warn(` Use the above URL or IP to access your application.`)
   Logger.empty()
+
 }
 
 module.exports = {
