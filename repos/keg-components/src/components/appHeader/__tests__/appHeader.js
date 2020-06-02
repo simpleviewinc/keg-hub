c39efc7e62e93e43604e4d6063ffc09f9bdee816
Michael Carolin <mpcarolin.dev@gmail.com>
2020-06-02 13:38:05 -0700

    Expo Peer Dependencies (#27)
    Co-authored-by: Michael Carolin <michael.carolin@simpleviewinc.com>

---
+++ repos/keg-components/src/components/appHeader/__tests__/appHeader.js
@@ -0,0 +1,21 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { theme } from '../../../theme'
+import { ReThemeProvider } from 're-theme'
+
+const { AppHeader } = require('../appHeader')
+
+describe('AppHeader', () => {
+  it('renders correctly', () => {
+    renderer.create(
+      <ReThemeProvider theme={theme} merge={false}>
+        <AppHeader 
+          shadow 
+          title={"Header Title"}
+          leftIcon={'beer'}
+        />
+      </ReThemeProvider>
+    )
+  })
+})
