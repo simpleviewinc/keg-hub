9ef55ef44f246074a68f2aacc054e4bd47168734
lancetipton <lancetipton04@gmail.com>
2020-06-23 23:25:54 -0700

    Updates   * Added local machine update script

---
+++ scripts/cli/updates/1.2.0.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Prints a message to the terminal through stderr
+keg_message(){
+  echo "[ KEG CLI ] $@" >&2
+  return
+}
+
+# Asks a question in the terminal
+keg_ask_question(){
+  keg_message "$1"
+  read -p "" INPUT;
+  local ANSWER="${INPUT:0:1}"
+
+  echo "$ANSWER"
+}
+
+# Check for docker-sync, then asks if you want to remove it
+keg_remove_docker_sync(){
+  if [[ -x "$(command -v docker-sync 2>/dev/null)" ]]; then
+    local ANSWER=$(keg_ask_question "Confirm remove docker-sync? (y/N):")
+    if [[ "$ANSWER" == "y" || "$ANSWER" == "Y" ]]; then
+      keg_message "Removing docker-sync..."
+      gem uninstall docker-sync
+      brew uninstall eugenmayer/dockersync/unox
+      echo ""
+    fi
+  fi
+}
+
+# Check for unison, then asks if you want to remove it
+keg_remove_unison(){
+  if [[ ! -z "$(which unison)" ]]; then
+    local ANSWER=$(keg_ask_question "Confirm remove unison ( Used for docker-sync )? (y/N):")
+    if [[ "$ANSWER" == "y" || "$ANSWER" == "Y" ]]; then
+      keg_message "Removing unison..."
+      brew uninstall unison
+      echo ""
+    fi
+  fi
+}
+
+# Check for mutagen, then installs it if it doesn't exist
+keg_install_mutagen(){
+  if [[ -x "$(command -v mutagen 2>/dev/null)" ]]; then
+    return
+  else
+    brew install mutagen-io/mutagen/mutagen
+    echo ""
+  fi
+}
+
+keg_cli_1_2_0_update(){
+
+  # Remove docker-sync from the local machine
+  keg_remove_docker_sync
+
+  # Remove unison from the local machine
+  # Its a dependecy of docker-sync
+  keg_remove_unison
+
+  # Install mutagen on the local machine
+  keg_install_mutagen
+
+  echo ""
+  keg_message "1.2.0 Update Complete!"
+  echo ""
+
+}
+
+# Run update
+keg_cli_1_2_0_update "$@"
