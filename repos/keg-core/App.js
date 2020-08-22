c27779dedfa31d17cd600be26015f7f8a9c873c6
Daniel Yo <dyo@simpleviewinc.com>
2020-08-21 18:15:21 -0700

    utilize webDimensions (#65)
    * update:
    - removed setRNPlatform to be able to use webDimensions by default
    
    * use latest re-theme v 2.1.1
    
    * ran yarn install

--- repos/keg-core/App.js
+++ repos/keg-core/App.js
@@ -1,10 +1,9 @@
 import 'SVUtils/overrides/console'
-import { Dimensions, Platform } from 'react-native'
-import { setRNDimensions, setRNPlatform } from '@simpleviewinc/re-theme'
+import { Platform } from 'react-native'
+import { setRNPlatform } from '@simpleviewinc/re-theme'
 import { enableScreens } from 'react-native-screens'
 import { App } from 'SVTap'
 
-setRNDimensions(Dimensions)
 setRNPlatform(Platform)
 enableScreens()
 
