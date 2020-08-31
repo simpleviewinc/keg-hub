835242c61fad93de81fd0b464156d1a8c6db61fb
lancetipton <lancetipton04@gmail.com>
2020-08-30 21:12:21 -0700

    Updates   * Added more tests

---
+++ repos/re-theme/src/theme/__tests__/manageTheme.js
@@ -0,0 +1,46 @@
+jest.resetModules()
+jest.resetAllMocks()
+jest.clearAllMocks()
+
+const { getCurrentTheme, updateCurrentTheme } = require('../manageTheme')
+
+describe('manageTheme', () => {
+
+  describe('updateCurrentTheme', () => {
+
+    afterEach(() => {
+      updateCurrentTheme({})
+    })
+
+    it('should override the default Theme', () => {
+
+      const testTheme = {}
+      const defTheme = getCurrentTheme()
+
+      expect(defTheme).not.toBe(testTheme)
+
+      updateCurrentTheme(testTheme)
+
+      expect(getCurrentTheme()).toBe(testTheme)
+
+    })
+
+  })
+
+  describe('getCurrentTheme', () => {
+
+    it('should return the default Theme, when no theme is set', () => {
+
+      const defTheme = getCurrentTheme()
+
+      expect(typeof defTheme).toBe('object')
+
+    })
+
+    afterEach(() => {
+      updateCurrentTheme({})
+    })
+
+  })
+
+})
