e38b0cee862de4ab764d68c31a7e38e40986ea33
Lance Tipton <lancetipton04@gmail.com>
2020-09-17 02:03:21 -0700

    Updates   * Added injector for stlyes   * Is a Hoc that wraps a componetns and injects the styles to the DOM   * Currently setup for the veiw component   * Added useStyleTag component

---
+++ repos/keg-components/src/components/view/view.injected.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { View as RNView } from 'react-native'
+import { useClassName } from 'KegClassName'
+import PropTypes from 'prop-types'
+import { StyleInjector } from 'KegInjector'
+
+/**
+ * View
+ * @summary Default view component that wraps the React Native View component. All props are optional
+ *
+ * @param {Object} props - see View PropTypes
+ * @property {String} props.className - Value to set the className to (web platform only)
+ *
+ */
+export const View = StyleInjector(
+  React.forwardRef(({ children, className, viewRef, ...props }, ref) => {
+
+  const classRef = useClassName('keg-view', className, ref)
+
+  return (
+    <RNView
+      {...props}
+      ref={classRef}
+    >
+      { children }
+    </RNView>
+  )
+}), { displayName: 'View', className: 'keg-view' })
+
+
+
+View.propTypes = {
+  ...RNView.propTypes,
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+  ])
+}
