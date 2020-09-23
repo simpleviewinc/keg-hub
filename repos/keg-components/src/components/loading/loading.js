de26102ec0e868f67e382e91b2580df7985c34cb
Daniel Yo <dyo@simpleviewinc.com>
2020-09-23 16:19:30 -0700

    keg-components - custom loading component (#37)
    * update:
    - added font-awesome spinner svg component for testing custom loading icon
    - updated jsdoc
    - updated fromToAnimation to allow looping
    
    * updated useFromTo dependency

--- repos/keg-components/src/components/loading/loading.js
+++ repos/keg-components/src/components/loading/loading.js
@@ -11,11 +11,12 @@ import { useThemePath } from 'KegHooks'
  * Progress
  * @summary Custom Progress component. All props are optional
  *
- * @param {*} props
+ * @param {Object} props
  * @property {String} props.text - button text
  * @property {Object} props.style - custom style
- * @property {Object} props.theme - App theme object
- *
+ * @property {Component} props.loadIndicator custom load indicator
+ * @property {Object=} props.type - Theme type to use
+ * @property {number|String=} props.size - Size of the loading indicator
  */
 const Progress = props => {
   const { styles, text, loadIndicator, type, size } = props
@@ -51,14 +52,14 @@ const Progress = props => {
  * @summary Custom Loading component. All props are optional
  *
  * @param {Object} props - see Loading.propTypes
- * @property {String} props.className - Class name of the root element
- * @property {String} props.text - Text to display while loading
- * @property {number} props.size - Size of the loading indicator
- * @property {Object} props.styles - Styles object tree
- * @property {Object} props.themePath - Custom path to styles on the theme
- * @property {Object} props.type - Theme type to use
- * @property {Object} props.children - Custom Children of the loading component
- *
+ * @property {String=} props.className - Class name of the root element
+ * @property {String=} props.text - Text to display while loading
+ * @property {number|String=} props.size - Size of the loading indicator
+ * @property {Object=} props.styles - Styles object tree
+ * @property {Object=} props.themePath - Custom path to styles on the theme
+ * @property {Object=} props.type - Theme type to use
+ * @property {Object=} props.children - Custom Children of the loading component
+ * @property {Component=} props.indicator - custom indicator
  */
 export const Loading = props => {
   const {
--- repos/keg-components/src/hooks/useFromToAnimation.js
+++ repos/keg-components/src/hooks/useFromToAnimation.js
@@ -1,7 +1,6 @@
 import { useEffect, useMemo } from 'react'
 import { Animated } from 'react-native'
 import { noOp } from 'KegUtils'
-import { isArr } from '@keg-hub/jsutils'
 
 /**
  * A hook for running an animation from an origin (from) point to a destination (to) point
@@ -9,25 +8,33 @@ import { isArr } from '@keg-hub/jsutils'
  * @param {number} params.from - the starting value of the animation
  * @param {number} params.to - the ending destination value of the animation
  * @param {Number} params.duration - time in milliseconds that the animation should proceed
+ * @param {boolean} params.loop - loop the animation
+ * @param {Easing} params.easing - react-native Easing obj
  * @param {Function} params.onFinish - passed to Animated.start(), called when the animation finishes
- * @param {Array} dependencies - optional array of dependencies that will dictate when the hook re-runs the animation.
- *                               By default, the animation reruns if the `from` or `to` values change.
  * @returns {Array} [ Animated.Value ]
  *  - Animated.Value: pass this value to your Animated.View to begin the animation
  */
-export const useFromToAnimation = (params, dependencies) => {
-  const { from, to, duration = 500, onFinish = noOp } = params || {}
-
+export const useFromToAnimation = params => {
+  const { from, to, duration = 500, onFinish = noOp, loop = false, easing } =
+    params || {}
   // determines when the animation should run
-  const animDependencies = isArr(dependencies) ? dependencies : [ from, to ]
+  const animDependencies = [ from, to, duration, loop, easing, onFinish ]
 
   // define the animated value here so we can return it. It needs to recompute
   // whenever the animation would run again, which is why it shares hookDependencies
   // with the useEffect below
   const fromVal = useMemo(() => new Animated.Value(from), animDependencies)
 
+  const animatedTiming = Animated.timing(fromVal, {
+    toValue: to,
+    duration,
+    easing,
+  })
+
   useEffect(() => {
-    Animated.timing(fromVal, { toValue: to, duration }).start(onFinish)
+    loop
+      ? Animated.loop(animatedTiming).start()
+      : animatedTiming.start(onFinish)
   }, animDependencies)
 
   return [fromVal]
