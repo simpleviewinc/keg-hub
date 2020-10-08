c23f12577b533874978f37dfc753b5bb3f2d3487
Daniel Yo <dnl992004@yahoo.com.au>
2020-10-08 12:48:56 -0700

    added the other noOp obj

--- repos/keg-core/core/base/utils/helpers/method/noop.js
+++ repos/keg-core/core/base/utils/helpers/method/noop.js
@@ -1,6 +1,13 @@
+import { deepFreeze } from '@keg-hub/jsutils'
 /**
  * @summary Empty function that does nothing
  *
  * @returns {function}
  */
-export const noOp = () => {}
+export const noOp = Object.freeze(() => {})
+
+export const noOpObj = Object.freeze({})
+
+export const noPropObj = deepFreeze({ content: {} })
+
+export const noPropArr = deepFreeze([])
