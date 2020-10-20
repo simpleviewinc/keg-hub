2566431832d0d165343a35e84d2ca945712c5fd5
mpcarolin <mpcarolin.dev@gmail.com>
2020-10-20 15:54:39 -0700

    added some jsdocs and optional chaining

--- repos/re-theme/src/styleParser/getCSSRules.js
+++ repos/re-theme/src/styleParser/getCSSRules.js
@@ -11,21 +11,21 @@ const isSecurityError = error => {
 /**
  * Helper for getCSSRules - handles the error arising from getting 
  * stylesheet cssRules property
- * @param {*} stylesheet 
+ * @param {Object} stylesheet 
  * @param {Error} error 
  */
 const warnOfStylesheetError = (stylesheet, error) => {
   if (isSecurityError(error)) {
     console.warn(
       'Cannot parse cross-origin restricted stylesheet from source:',
-      stylesheet.href,
+      stylesheet?.href,
       '\nThis stylesheet will be safely ignored, but if you need access to it, then you should either ensure the domains match or adjust the stylesheet server\'s security policy.'
     )
   }
   else {
     console.warn(
       'Cannot parse stylesheet from source:',
-      stylesheet.href,
+      stylesheet?.href,
       `\n Reason: ${error.name} - ${error.message}`, 
     )
   }
