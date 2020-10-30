3455ca63248aaebe89f4c0f98867e57e71f94504
Daniel Yo <dnl992004@yahoo.com.au>
2020-10-30 10:56:43 -0700

    update: - update hook folder structure so rollup alias config can pick it up

---
+++ repos/keg-components/src/hooks/useAnimate/useAnimate.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react'
+import { isFunc } from '@keg-hub/jsutils'
+
+export const useAnimate = ({ ref, animation, config, startCb, startDelay }) => {
+  const aniRef = useRef(ref)
+
+  const animate = () => {
+    const element = aniRef.current
+    element && isFunc(element.animate) && element.animate(animation, config)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => animate(), startDelay || 0)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return [aniRef]
+}
---
+++ repos/keg-components/src/hooks/useClassList/useClassList.native.js
@@ -0,0 +1,11 @@
+import { noPropArr } from '@keg-hub/jsutils'
+
+/**
+ * Placeholder hook for Native platforms
+ * <br/>Just returns an empty array
+ *
+ * @returns {Array} - Empty array
+ */
+export const useClassList = () => {
+  return noPropArr
+}
