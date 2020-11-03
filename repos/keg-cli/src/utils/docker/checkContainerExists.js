05afacdf89f461f747d95888a51626d11e1c29cd
Lance Tipton <lancetipton04@gmail.com>
2020-11-02 20:22:21 -0700

    Updates   * Cleaned up checkContainerExists logic   * Updated to call stop service instead of destroy

--- repos/keg-cli/src/utils/docker/checkContainerExists.js
+++ repos/keg-cli/src/utils/docker/checkContainerExists.js
@@ -1,7 +1,7 @@
 const docker = require('KegDocCli')
 const { Logger } = require('KegLog')
 const { ask } = require('@keg-hub/ask-it')
-const { destroyService } = require('KegUtils/services/destroyService')
+const { stopService } = require('KegUtils/services/stopService')
 
 /**
  * Checks if a docker container already exists
@@ -14,21 +14,21 @@ const { destroyService } = require('KegUtils/services/destroyService')
 const checkContainerExists = async (containerRef, context, args) => {
   const exists = await docker.container.get(containerRef)
   if(!exists) return false
-  
+
   Logger.empty()
   Logger.log(
-    Logger.colors.brightYellow(`A docker container already exists with the name`),
+    Logger.colors.brightYellow(`A docker container already exists with the same name or domain:`),
     Logger.colors.cyan(`"${containerRef}"`),
-    Logger.colors.brightRed(`\nRunning container must be removed before this container can be run!`)
+    Logger.colors.brightRed(`\nThe existing container must be stopped before this container can be started!`)
   )
 
   Logger.empty()
-  const remove = await ask.confirm(`Would you like to remove it?`)
+  const remove = await ask.confirm(`Would you like to stop it?`)
   Logger.empty()
 
   if(!remove) return exists.name
-
-  await destroyService({ ...args }, { context: context, container: containerRef })
+ 
+  await stopService({ ...args }, { context: context, container: containerRef })
 
   return false
 }
