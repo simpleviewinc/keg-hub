3f0ec761000db8ef431d1b64e951a21e6450a96a
Lance Tipton <lancetipton04@gmail.com>
2020-11-20 10:00:19 -0700

    Updates   * Added image list mock for docker mocks   * Updated contianer select to use ENV constants   * Added tests for containerSelect and imageSelect

--- repos/keg-cli/src/utils/docker/containerSelect.js
+++ repos/keg-cli/src/utils/docker/containerSelect.js
@@ -1,28 +1,9 @@
 const { ask } = require('@keg-hub/ask-it')
 const docker = require('KegDocCli')
-const { checkCall } = require('@keg-hub/jsutils')
+const { checkCall, get } = require('@keg-hub/jsutils')
 const { throwNoContainers } = require('../error/throwNoContainers')
-
-/**
- * @param {string} labels - all labels stored as a single csv string
- * @return {string} the domain label value
- */
-const getDomain = labels => {
-  const split = labels 
-    ? labels.split(',')
-    : []
-
-  const domain = split.reduce((result, next) => {
-    if (result) return result
-    const [ key, value ] = next.split('=')
-    return (key === 'com.keg.proxy.domain')
-      ? value
-      : result
-  }, null)
-
-  // return the domain, or if not found, the proxy's dashboard
-  return domain || 'local.kegdev.xyz'
-}
+const { kegLabelKeys } = require('KegConst/docker/labels')
+const { KEG_ENVS } = require('KegConst/envs')
 
 /**
  * Prompts user to select a container from the current docker containers
@@ -35,17 +16,16 @@ const containerSelect = async (filter, throwError=true) => {
 
   // If no containers available, then check if we should call throwError
   // Otherwise just return null
-  if(!containers.length){
+  if(!containers.length)
     return throwError
       ? throwNoContainers()
       : null
-  }
 
   // Filter the containers if filter method passed
   const filtered = checkCall(filter, containers) || containers
 
   // Get a string of each container to print to terminal
-  const items = filtered.map(cont => `${cont.name} | ${ cont.image } | ${ getDomain(cont.labels) } | ${ cont.id }`)
+  const items = filtered.map(cont => `${cont.name} | ${ cont.image } | ${ get(cont, ['labelsObj', kegLabelKeys.KEG_PROXY_DOMAIN], KEG_ENVS.KEG_PROXY_HOST) } | ${ cont.id }`)
 
   const index = await ask.promptList(
     items,
