6e9970fea06b17fc4c4c56b60551d5f8462472f4
Daniel Yo <dyo@simpleviewinc.com>
2020-12-08 17:06:46 -0700

    keg-cli: delete downloaded images (#89)
    [Semver-Status] minor
    
    Updates
    * Updated showHelp to display options on sub items if exists
    * Added constant for docker package url
    * Removed unused import
    * Added option remote for keg di rm

--- repos/keg-cli/src/libs/docker/image.js
+++ repos/keg-cli/src/libs/docker/image.js
@@ -146,9 +146,9 @@ const getByTag = async (imgRef, log=false) => {
 /**
  * Removes a docker image based on passed in toRemove argument
  * @function
-* @param {string} args - Arguments used in the docker remove command
+ * @param {string} args - Arguments used in the docker remove command
  *
- * @returns {string} - Response from the docker cli command
+ * @returns {Promise<string|Array>} - Response from the docker cli command
  */
 const removeImage = args => {
   return remove({ ...args, type: 'image' })
