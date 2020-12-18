92d9016c3ac6125d53bbaa933bc0484e8a6e69b2
lancetipton <lancetipton04@gmail.com>
2020-12-18 14:39:51 -0700

    Updates   * More keg-base clean up

---
+++ repos/keg-cli/containers/base/keg-exec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load the keg-cli
+source /keg-hub/repos/keg-cli/keg
+
+# Call the keg with the passed in arguments
+keg "$@"
--- repos/keg-cli/src/libs/git/__tests__/branch.js
+++ repos/keg-cli/src/libs/git/__tests__/branch.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const { isArr } = require('@keg-hub/jsutils')
-const cliRoot = path.join(__dirname, '../../../../')
+const kegRoot = path.join(__dirname, '../../../../../')
 const { git } = require('../git')
 
 
@@ -12,7 +12,7 @@ describe('branch', () => {
 
     it('should return a list of all branches for the passed in location', async done => {
 
-      const res = await git.branch.list(cliRoot)
+      const res = await git.branch.list(kegRoot)
 
       expect(isArr(res)).toBe(true)
 
@@ -21,7 +21,7 @@ describe('branch', () => {
 
     it('should return an array of  branch objects matching the correct model', async done => {
 
-      const res = await git.branch.list(cliRoot)
+      const res = await git.branch.list(kegRoot)
       const keys = [ 'commit', 'name', 'current', 'message' ]
 
       res.map(branch => keys.map(key => expect(key in branch).toBe(true)))
