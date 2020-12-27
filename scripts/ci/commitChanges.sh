6e6c79b6ed769767245e3fcd2c74cf221b3de925
lancetipton <lancetipton04@gmail.com>
2020-12-27 01:47:54 -0700

    Updates   * Cleaned up auto-commit message

--- scripts/ci/commitChanges.sh
+++ scripts/ci/commitChanges.sh
@@ -2,11 +2,19 @@
 
 # Auto commit changes to repo after yarn command is run
 keg_commit_changes(){
+
+  local MESSAGE=""
+  if [[ -z "$1" ]]; then
+    MESSAGE="( Auto-Commit )"
+  else
+    MESSAGE="$1 ( Automatic-Commit )"
+  fi
+
   # Get the last users email / name for the commit message
   git config --local user.email "$(git log --format='%ae' HEAD^!)"
   git config --local user.name "$(git log --format='%an' HEAD^!)"
   git add  -A
-  git commit -m "Github Actions - Auto-Format" --allow-empty
+  git commit -m "Github Actions - $MESSAGE" --allow-empty
   git push -u origin HEAD
 }
 
