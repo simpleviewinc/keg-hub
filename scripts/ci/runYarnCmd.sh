a1ca7c273c362552a0d3f55faea068f409515c9b
lancetipton <lancetipton04@gmail.com>
2021-01-04 17:34:18 -0700

    feat: Added github actions for keg-hub   * Actions added     * Build - Runs on push to master     * Format - Runs on push to develop     * PR-Changes - Runs on pull_request     * Tests - Runs on pull_request   * Added helper scripts for running the actions

---
+++ scripts/ci/runYarnCmd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Load the repos to run the command on
+REPOS=($(cat ../keg-changed-repos.txt))
+
+echo "::debug::Found Repos $REPOS"
+
+# Loop over the repos and run the passed in command on them
+keg_run_yarn_cmd(){
+  for REPO_PATH in "${REPOS[@]}"; do
+    echo "::debug::Runing cmd for repo $REPOS"
+  
+    cd $REPO_PATH
+    for CMD in "$@"; do
+      if [[ "$CMD" ]]; then
+        yarn "$CMD"
+      fi
+    done
+  done
+}
+
+keg_run_yarn_cmd "$@"
