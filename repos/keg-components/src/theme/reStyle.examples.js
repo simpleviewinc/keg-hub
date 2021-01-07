107a2e8fb6581c378520eb06f8aecdb03e567365
lancetipton <lancetipton04@gmail.com>
2021-01-07 11:32:25 -0700

    docs: Added docs for reStyle with Keg-Components

---
+++ repos/keg-components/src/theme/reStyle.examples.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+import React from 'react'
+import { Text as KegText } from '../components/typography'
+import { Button as KegButton } from '../components/button'
+import { reStyle } from '@keg-hub/re-theme/reStyle'
+
+const Text = reStyle(KegText)({
+  color: 'green',
+  textAlign: 'center',
+})
+
+export const GreenText = () => (<Text>Green Text</Text>)
+
+const ReStyleHover = reStyle(
+  KegButton,
+  'styles'
+)((theme, props) => ({
+  default: {
+    main: {
+      backgroundColor: theme.colors.palette.blue03,
+    },
+  },
+  hover: {
+    main: {
+      backgroundColor: props.hoverColor,
+    },
+  },
+}))
+
+export const HoverButton = () => {
+  return (
+    <ReStyleHover content={'Hover Me'} hoverColor={`#02b4a3`} />
+  )
+}
