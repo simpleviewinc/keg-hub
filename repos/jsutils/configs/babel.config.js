9d488c5a16fd3c539dec9f1dadbc96c0471f116c
Daniel Yo <dyo@simpleviewinc.com>
2021-01-07 15:30:32 -0700

    update plugin terser to latest version (#118)
    `[semver-status]`: patch
    
    * update plugin terser to latest version
    
    * update:
    - added plugin for nullish operator
    - added plugin for optional chaining

--- repos/jsutils/configs/babel.config.js
+++ repos/jsutils/configs/babel.config.js
@@ -2,5 +2,9 @@ module.exports = {
   presets: [
     [ '@babel/preset-env', { targets: { node: 'current' } } ],
     [ '@babel/preset-react' ]
+  ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining'
   ]
 }
