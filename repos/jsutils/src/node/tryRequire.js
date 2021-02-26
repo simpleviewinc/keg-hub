3af00b30d9a82f138b46acc6009358cca55b6fec
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-25 17:40:55 -0700

    updated docs

--- repos/jsutils/src/node/tryRequire.js
+++ repos/jsutils/src/node/tryRequire.js
@@ -5,8 +5,12 @@ const fs = require('fs')
 /**
  * Tries to require the path, returning null if unable to.
  * Does not throw.
+ * @function
  * @param {string} filePath - path to file. You should use an absolute path
  * @return {*?} the export at path, if it exists, null otherwise
+ * @example
+ * const module = tryRequire('/keg/tap/foo/bar.js')
+ * if (!module) console.log('bar.js module does not exist')
  */
 const tryRequire = filePath => {
   try {
