14664b36cf9ffde25f198c3ce9283f85f03b3896
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-26 13:49:49 -0700

    removed cmd module

--- repos/jsutils/src/node/index.js
+++ repos/jsutils/src/node/index.js
@@ -1,11 +1,9 @@
 const loadModule = require('./loadModule')
-const cmd = require('./cmd')
 const universalMethods = require('../../build/cjs/index')
 const tryRequire = require('./tryRequire')
 
 module.exports = {
   ...universalMethods,
-  ...cmd,
   ...tryRequire,
   loadModule,
 }
