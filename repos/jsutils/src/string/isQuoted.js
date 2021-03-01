aa007e4f8691335d113645130ba5f48c8f81eb3b
mpcarolin <mpcarolin.dev@gmail.com>
2021-03-01 13:00:26 -0700

    made isQuoted more reusable

--- repos/jsutils/src/string/isQuoted.js
+++ repos/jsutils/src/string/isQuoted.js
@@ -1,18 +1,25 @@
 /** @module string */
 import { isStr } from './isStr'
 
+const quoteSymbols = [
+  '\"',
+  '\'',
+]
+
 /**
  * Checks if the string contains quoted text
  * @function
- * @param {string} str 
+ * @param {string} str - string to check
+ * @param {Array<string>?} quotes - optional array of valid quote strings to check with. Defaults to single and double quote characters.
  * @return {boolean} true if `str` is a quoted string
  * @example
  * isQuoted('foo') // false
  * @example
  * isQuoted('"foo"') // true
  */
-export const isQuoted = str => {
+export const isQuoted = (str, quotes=quoteSymbols) => {
   return isStr(str) && 
-    str.startsWith('"') &&
-    str.endsWith('"')
+    quotes.some(
+      quote => str.startsWith(quote) && str.endsWith(quote)
+    )
 }
