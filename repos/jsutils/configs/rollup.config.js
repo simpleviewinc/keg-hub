f3e76a031354fd62a5c35f5ff5358c9a2eb7c174
lancetipton <lancetipton04@gmail.com>
2021-03-15 17:09:28 -0700

    feat(dependencies): Updated package.json Dependencies   * Rollup   * Babel   * Expo   * React-Native(-Web)   * Jest * Updated yarn.lock files * Ran tests, and all pass for all repos

--- repos/jsutils/configs/rollup.config.js
+++ repos/jsutils/configs/rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
