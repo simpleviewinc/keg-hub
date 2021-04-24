02cbe6ee77ba6bb338df9e1a93170d72926583ab
Lance Tipton <lancetipton04@gmail.com>
2021-04-23 17:48:47 -0700

    Zen 609 automate package exec (#152)
    [Semver-Status] patch
    
    * feat(package.json): Web Builds
      * Updated package.json to allow production web-builds to be served properly
      * Added script for running the expo-optimize pacakge with npx
      * Updated build:web script to include expo-optimize script
    
    * feat(actions): Tap Actions
      * Added build and serve actions to retheme
      * Added NODE_ENV to scripts in keg-components and re-theme
      * Updated the sb:server script to match the serve cmd from actions
    
    * feat(retheme): Serve bundle
    * Updated ReThemes container files to allow serving the bundled app
    * Added new DOC_BUILD_PATH env
    
    * fix(retheme): Fix bugs in retheme's run.sh file

--- repos/re-theme/container/run.sh
+++ repos/re-theme/container/run.sh
@@ -1,31 +1,25 @@
 #!/usr/bin/env
 
+# Hides annoying story-book warnings
 export SUPPRESS_SUPPORT=1
+
+# Ensure required envs are set
+[[ -z "$KEG_PROXY_PORT" ]] && KEG_PROXY_PORT=60710
+[[ -z "$DOC_APP_PATH" ]] && DOC_APP_PATH=/keg/tap
+[[ -z "$DOC_BUILD_PATH" ]] && DOC_BUILD_PATH=/keg/tap-build
 
 # If the no KEG_DOCKER_EXEC env is set, just sleep forever
 # This is to keep our container running forever
-if [[ -z "$KEG_DOCKER_EXEC" ]]; then
-  tail -f /dev/null
-  exit 0
+[[ -z "$KEG_DOCKER_EXEC" ]] && tail -f /dev/null && exit 0;
 
-else
-
-  # Ensure the DOC_APP_PATH is set
-  if [[ -z "$DOC_APP_PATH" ]]; then
-    DOC_APP_PATH=/keg/tap
-  fi
-
-  # cd into the tap repo
-  cd $DOC_APP_PATH
-
-  # Check if no exect command exists, or if it's set to web
-  # Then default it to run the re-theme app
-  if [[ -z "$KEG_EXEC_CMD" || "$KEG_EXEC_CMD" == 'web' ]]; then
-    KEG_EXEC_CMD="sb"
-  fi
-
-  # Start the tap instance
-  echo $"[ KEG-CLI ] Running command 'yarn $KEG_EXEC_CMD'" >&2
-  yarn $KEG_EXEC_CMD
-
+# Check the NODE_ENV, and use that to know which environment to start
+# For non-development environments, we want to serve the bundle if it exists
+if [[ ! " development develop local test " =~ " $NODE_ENV " ]]; then
+  [[ -d "$DOC_BUILD_PATH" ]] && npx serve $DOC_BUILD_PATH --cors -n -l $KEG_PROXY_PORT && exit 0;
+  echo $"[ KEG-CLI ] Serve path $DOC_BUILD_PATH does not exist!" >&2
 fi
+
+# Serve the app bundle in development environemnts
+echo $"[ KEG-CLI ] Running development server!" >&2
+cd $DOC_APP_PATH
+[[ -z "$KEG_EXEC_CMD" || "$KEG_EXEC_CMD" == 'web' ]] && yarn sb || yarn $KEG_EXEC_CMD
