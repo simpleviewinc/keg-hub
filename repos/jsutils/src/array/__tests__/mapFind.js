8ea5f74774751cf8e9b4a73c64cedd0275f194e8
mpcarolin <mpcarolin.dev@gmail.com>
2021-05-10 16:26:30 -0700

    enhanced mapFind to work with objects; updated tests

--- repos/jsutils/src/array/__tests__/mapFind.js
+++ repos/jsutils/src/array/__tests__/mapFind.js
@@ -1,21 +1,24 @@
 import { mapFind } from '../'
 describe('mapFind', () => {
+
   it('It should find the first defined mapped value when no test function is specified', () => {
     const elements = [1,2,3]
     const expected = {}
     const result = mapFind(elements, x => x === 2 ? expected : null)
     expect(result).toBe(expected)
   })
+
   it('should find the first mapped value passing the test function', () => {
     const elements = [1,2,3]
     const expected = 9
     const result = mapFind(
       elements, 
       x => x ** 2,
-      x => x === 9
+      (squared, idx) => squared === 9 && idx === 2
     )
     expect(result).toBe(expected)
   })
+
   it('should return null for no match', () => {
     const elements = [1,2,3]
     const result = mapFind(
@@ -25,5 +28,24 @@ describe('mapFind', () => {
     )
     expect(result).toBe(null)
 
+  })
+
+  it('should work with objects', () => {
+    const elements = {
+      a: 1,
+      b: 2,
+      c: 3
+    }
+    const result = mapFind(
+      elements, 
+      (num, key, idx) => ({ input: num, output: num ** 2, key, idx }),
+      ({ output }) => output === 9
+    )
+    expect(result).toEqual({
+      input: 3,
+      output: 9,
+      key: 'c',
+      idx: 2
+    })
   })
 })
--- repos/jsutils/src/array/mapFind.js
+++ repos/jsutils/src/array/mapFind.js
@@ -1,16 +1,62 @@
-import { isArr } from './isArr'
+import { isColl } from '../collection/isColl'
+import { isObj } from '../object/isObj'
 import { isFunc } from '../method/isFunc'
 import { exists } from '../ext/exists'
 import { validate } from '../validation'
 
 /**
- * Finds the first element in arr whose mapped value passes the test function, then returns
+ * Helper for mapFind, handling the array case
+ * @param {Array} arr 
+ * @param {Function} mapper 
+ * @param {Function} test 
+ * @returns {*}
+ */
+const mapFindArr = (arr, mapper, test) => {
+  // iterate over each value in the array,
+  // returning when a mapped value is found that passes `test`
+  for (let i = 0; i < arr.length; i++) {
+    const mappedValue = mapper(arr[i], i, i)    
+    if (test(mappedValue, i, i))
+      return mappedValue
+  }
+
+  return null 
+}
+
+/**
+ * Helper for mapFind, handling the object case
+ * @param {Object} obj 
+ * @param {Function} mapper 
+ * @param {Function} test 
+ * @returns {*}
+ */
+const mapFindObj = (obj, mapper, test) => {
+  let idx = 0
+
+  // iterate over each property in the object
+  // returning when a mapped value is found that passes `test`
+  for (let key in obj) {
+    if (!obj.hasOwnProperty(key)) continue
+
+    const value = obj[key]
+    const mappedValue = mapper(value, key, idx)
+    if (test(mappedValue, key, idx)) 
+      return mappedValue
+
+    idx++
+  }
+
+  return null  
+}
+
+/**
+ * Finds the first element in coll whose mapped value passes the test function, then returns
  * the **mapped** value.
- * It will not map the entire array, only the subset needed to find the first passing element.
+ * It will not map the entire array or object; only the subset needed to find the first passing element.
  * @function
- * @param {Array} arr - array of elements to map and find
- * @param {Function} mapper - mapping function
- * @param {Function?} test - predicate fn for the mapped value. Defaults to checking if it is defined.
+ * @param {Array|Object} coll - elements to map and find
+ * @param {Function} mapper - mapping function of form: (value, key, idx) -> *. "key" is the index when coll is an array. "idx" is the index of the array value or object entry.
+ * @param {Function?} test - predicate function of form: (mappedValue, key, idx) -> true/false. Defaults to checking if the mapped value is defined. "key" is the index when coll is an array.
  * @returns {*?} the first passing mapped value
  * 
  * @example 
@@ -22,15 +68,17 @@ import { validate } from '../validation'
  * // Find the first file path whose required value is an object
  * const filePaths = [...]
  * const loadedFile = mapFind(filePaths, tryRequireSync, isObj)
+ * 
+ * @example
+ * // Find the first file path whose required value is an object
+ * const filePaths = { document: "foo/bar/doc.txt", image: "foo/bar/pic.img"}
+ * const loadedFile = mapFind(filePaths, (value, key) => tryRequireSync(value), isObj)
  */
-export const mapFind = (arr, mapper, test=exists) => {
-  const [ valid ] = validate({ arr, mapper, test }, { arr: isArr, $default: isFunc })
+export const mapFind = (coll, mapper, test=exists) => {
+  const [ valid ] = validate({ coll, mapper, test }, { coll: isColl, $default: isFunc })
   if (!valid) return undefined
 
-  for (let i = 0; i < arr.length; i++) {
-    const value = mapper(arr[i])    
-    if (test(value)) return value
-  }
-
-  return null
+  return isObj(coll)
+    ? mapFindObj(coll, mapper, test)
+    : mapFindArr(coll, mapper, test)
 }
