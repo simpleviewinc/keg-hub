6a5acbec95993e2f80c10d36885a9dc5b4e78b47
lancetipton <lancetipton04@gmail.com>
2021-08-09 18:11:59 -0700

    fix(retheme): Fix bable config * Add plugins to remove warning

--- repos/re-theme/configs/babel.config.js
+++ repos/re-theme/configs/babel.config.js
@@ -6,8 +6,10 @@ module.exports = {
   plugins: [
     [
       "@babel/plugin-proposal-class-properties",
-      { loose: true } 
+      { loose: true }
     ],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
     ['transform-react-remove-prop-types', { removeImport: 'true' }],
   ]
 }
