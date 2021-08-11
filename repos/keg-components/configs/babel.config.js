91308653223e1a4f69d8b609b45a33135e59b94a
lancetipton <lancetipton04@gmail.com>
2021-08-11 12:42:33 -0700

    fix(components): Fix warning from bable config

--- repos/keg-components/configs/babel.config.js
+++ repos/keg-components/configs/babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: [ 
-    ['@babel/preset-env', { loose: true }],
-    ['@babel/preset-react', { loose: true }]
+    ['@babel/preset-env'],
+    ['@babel/preset-react']
   ],
   plugins: [
     ['@babel/plugin-proposal-optional-chaining'],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    ['@babel/plugin-proposal-class-properties'],
+    ["@babel/plugin-proposal-private-methods"],
+    ["@babel/plugin-proposal-private-property-in-object"],
     ['transform-react-remove-prop-types', { removeImport: 'true' }],
   ],
 }
+
+// ['@babel/preset-env', { loose: true, shippedProposals: true }],
