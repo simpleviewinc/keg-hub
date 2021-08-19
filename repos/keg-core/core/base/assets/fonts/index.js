1814c4bbc619e613e479f9ac759ddc29a86c62f5
lancetipton <lancetipton04@gmail.com>
2021-08-19 15:08:40 -0700

    Updating keg to version 9.3.0

---
+++ repos/keg-core/core/base/assets/fonts/index.js
@@ -0,0 +1,8 @@
+
+// ********************************* WARNING *********************************
+// This is an Auto-Generated file. Do NOT modify, changes WILL be overwritten!
+// ********************************* WARNING *********************************
+
+export const fonts = {
+
+}
