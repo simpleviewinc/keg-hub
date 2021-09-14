f1c70e78d84a99e427593deb4da76b0e5f8ab4ce
mpcarolin <mpcarolin.dev@gmail.com>
2021-09-14 16:34:49 -0700

    Docs update

--- repos/keg-components/src/components/svgIcon/svgIcon.native.js
+++ repos/keg-components/src/components/svgIcon/svgIcon.native.js
@@ -100,7 +100,7 @@ const useClassName = className => {
  *                              - Used as the stroke color when no stroke prop is passed
  *                              - Used as the fill color when no fill prop is passed
  * @param {string=} props.clipRule - Svg rule for clipping
- * @param {string=} props.delta - Path definition for the Svg Component
+ * @param {string=} props.delta - Path definition for the Svg Component. If omitted, no Path element is rendered.
  * @param {string=} props.fill - Fill color of the path element
  * @param {string=} props.fillRule - Svg rule for the fill attribute
  * @param {string=} props.size - Size of the Svg Component
